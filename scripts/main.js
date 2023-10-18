d08e2c6a86ce406501632fa2261271f41c88a344
Kaberna Irene <irinakabernaa@gmail.com>
2023-10-18 22:18:55 +0300

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+const numOrStr = prompt('input number or string');
+
+switch (numOrStr) {
+    case null:
+        alert ('ви скасували');
+        break;
+    case "":
+        alert('Empty String');
+        break;
+    case !isNaN( numOrStr ) || numOrStr:
+        alert(' number is Ba_NaN');
+        break;
+    default: 
+        alert('OK!');
+}
+
+
+
+
+
+
+
+
+
+
+
+
